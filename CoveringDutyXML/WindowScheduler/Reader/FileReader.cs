7a35b87da2a1081993502b0f73d74181c3bcc203
Tarsem Singh <tarsem.it@gmail.com>
2023-02-16 13:29:59 +0530

    added copy code

--- CoveringDutyXML/WindowScheduler/Reader/FileReader.cs
+++ CoveringDutyXML/WindowScheduler/Reader/FileReader.cs
@@ -23,82 +23,114 @@ public class FileReader
     {
         _logger.LogInformation("Running Import Operation");
         ImportConfig importConfig = ReadImportConfig();
-        DirectoryInfo folder = new DirectoryInfo(importConfig.FolderPath);
+        DirectoryInfo folder = new DirectoryInfo(importConfig.InboundFolder);
         var files = folder.GetFiles(importConfig.SearchPattern);
-        foreach (var file in files)
+        try
         {
-            XmlReaderSettings settings = new XmlReaderSettings();
-            settings.ConformanceLevel = ConformanceLevel.Fragment;
-            using var reader = XmlReader.Create(file.FullName, settings);
-
-            XDocument xdocument = XDocument.Load(file.FullName);
-            IEnumerable<XElement> employees = xdocument.Root.Elements().Where(e=>e.Name == "Employee");
-            foreach (var employee in employees)
+            foreach (var file in files)
             {
-                var dto = new EmployeeDto
+                 
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.ConformanceLevel = ConformanceLevel.Fragment;
+                using var reader = XmlReader.Create(file.FullName, settings);
+                   
+                var xdocument = XDocument.Load(file.FullName);
+                IEnumerable<XElement> employees = xdocument.Root.Elements().Where(e => e.Name == "Employee");
+                foreach (var employee in employees)
                 {
-                    UserId = employee.Element("USER_ID").Value,
-                    SOEID = employee.Element("SOE_ID").Value,
-                    EMAIL = employee.Element("EMAIL").Value,
-                    NRIC = employee.Element("NRIC").Value,
-                    DIDEXTENSION = employee.Element("DID_EXTENSION").Value,
-                    HANDPHONE = employee.Element("HAND_PHONE").Value,
-                    FAXNUMBER = employee.Element("FAX_NUMBER").Value,
-                    FIRSTNAME = employee.Element("FIRST_NAME").Value,
-                    LASTNAME = employee.Element("LAST_NAME").Value,
-                    KNOWNAS =  employee.Element("KNOWN_AS").Value,
-                    NAME = employee.Element("NAME").Value,
-                    POSITIONID = employee.Element("POSITION_ID").Value,
-                    POSITIONNAME = employee.Element("POSITION_NAME").Value,
-                    LOCATION = employee.Element("LOCATION").Value,
-                    EMPLOYEETYPE = employee.Element("EMPLOYEE_TYPE").Value,
-                    ROEMAIL = employee.Element("RO_EMAIL").Value,
-                    WORKUNITNAME = employee.Element("WORK_UNIT_NAME").Value,
-                    WORKUNITCODE = employee.Element("WORK_UNIT_CODE").Value,
-                    SECTIONNAME = employee.Element("SECTION_NAME").Value,
-                    SECTIONCODE = employee.Element("SECTION_CODE").Value,
-                    BRANCHNAME = employee.Element("BRANCH_NAME").Value,
-                    BRANCHCODE = employee.Element("BRANCH_CODE").Value,
-                    DIVISIONNAME = employee.Element("DIVISION_NAME").Value,
-                    DIVISIONCODE = employee.Element("DIVISION_CODE").Value,
-                    CLUSTERNAME = employee.Element("CLUSTER_NAME").Value,
-                    CLUSTERCODE = employee.Element("CLUSTER_CODE").Value,
-                    BOARDNAME = employee.Element("BOARD_NAME").Value,
-                    BOARDCODE = employee.Element("BOARD_CODE").Value,
-                    TERMINATIONDATE = employee.Element("TERMINATION_DATE").Value
-                };
-                var model = Util.Map(dto);
-                await _context.Upsert(model);
+                    try {
+                       
+                    var dto = new EmployeeDto
+                    {
+                        UserId = employee.Element("USER_ID").Value,
+                        SOEID = employee.Element("SOE_ID").Value,
+                        EMAIL = employee.Element("EMAIL").Value,
+                        NRIC = employee.Element("NRIC").Value,
+                        DIDEXTENSION = employee.Element("DID_EXTENSION").Value,
+                        HANDPHONE = employee.Element("HAND_PHONE").Value,
+                        FAXNUMBER = employee.Element("FAX_NUMBER").Value,
+                        FIRSTNAME = employee.Element("FIRST_NAME").Value,
+                        LASTNAME = employee.Element("LAST_NAME").Value,
+                        KNOWNAS = employee.Element("KNOWN_AS").Value,
+                        NAME = employee.Element("NAME").Value,
+                        POSITIONID = employee.Element("POSITION_ID").Value,
+                        POSITIONNAME = employee.Element("POSITION_NAME").Value,
+                        LOCATION = employee.Element("LOCATION").Value,
+                        EMPLOYEETYPE = employee.Element("EMPLOYEE_TYPE").Value,
+                        ROEMAIL = employee.Element("RO_EMAIL").Value,
+                        WORKUNITNAME = employee.Element("WORK_UNIT_NAME").Value,
+                        WORKUNITCODE = employee.Element("WORK_UNIT_CODE").Value,
+                        SECTIONNAME = employee.Element("SECTION_NAME").Value,
+                        SECTIONCODE = employee.Element("SECTION_CODE").Value,
+                        BRANCHNAME = employee.Element("BRANCH_NAME").Value,
+                        BRANCHCODE = employee.Element("BRANCH_CODE").Value,
+                        DIVISIONNAME = employee.Element("DIVISION_NAME").Value,
+                        DIVISIONCODE = employee.Element("DIVISION_CODE").Value,
+                        CLUSTERNAME = employee.Element("CLUSTER_NAME").Value,
+                        CLUSTERCODE = employee.Element("CLUSTER_CODE").Value,
+                        BOARDNAME = employee.Element("BOARD_NAME").Value,
+                        BOARDCODE = employee.Element("BOARD_CODE").Value,
+                        TERMINATIONDATE = employee.Element("TERMINATION_DATE").Value
+                    };
+                    var model = Util.Map(dto);
+                    await _context.Upsert(model);
+                    } catch (Exception ex ) {
+                        throw ex;
+                    }
+                }
+                
+                MoveFile(importConfig.ProcessFolder, file);
+
+                //  while (reader.ReadToFollowing("Employee"))
+                // {
+                //     var _dto = new EmployeeDto();
+                //     reader.ReadToFollowing("EMAIL");
+
+                //     // _dto.EMAIL = reader.ReadElementContentAsString();
+                //     Console.WriteLine($"Email: {reader.ReadNodeValue("Email")}");
+
+                //     reader.ReadToFollowing("NRIC");
+                //     Console.WriteLine($"author: {reader.ReadElementContentAsString()}");
+
+                //     reader.ReadToFollowing("USER_ID");
+                //     Console.WriteLine($"price: {reader.ReadElementContentAsString()}");
+
+                //     Console.WriteLine("-------------------------");
+                // } 
+
             }
-
-            //  while (reader.ReadToFollowing("Employee"))
-            // {
-            //     var _dto = new EmployeeDto();
-            //     reader.ReadToFollowing("EMAIL");
-
-            //     // _dto.EMAIL = reader.ReadElementContentAsString();
-            //     Console.WriteLine($"Email: {reader.ReadNodeValue("Email")}");
-
-            //     reader.ReadToFollowing("NRIC");
-            //     Console.WriteLine($"author: {reader.ReadElementContentAsString()}");
-
-            //     reader.ReadToFollowing("USER_ID");
-            //     Console.WriteLine($"price: {reader.ReadElementContentAsString()}");
-
-            //     Console.WriteLine("-------------------------");
-            // } 
-
         }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        
+    }
+
+    private static void MoveFile(string folder, FileInfo file)
+    {
+        if (!Directory.Exists(folder))
+        {
+            Directory.CreateDirectory(folder);
+        }
+        string fileName = $"{DateTime.Today.ToString("yyyyMMdd")}_{file.Name}";
+
+        string archievePath = Path.Combine(folder, fileName);
+        File.Copy(file.FullName, archievePath);
+        //File.Delete(file.FullName);
     }
 
     private ImportConfig ReadImportConfig()
     {
-        var folderPath = _configuration["ImportConfig:FolderPath"];
+        var folderPath = _configuration["ImportConfig:InboundFolder"];
+        var archive = _configuration["ImportConfig:ProcessFolder"];
         var searchPattern = _configuration["ImportConfig:SearchPattern"];
         return new ImportConfig
         {
-            FolderPath = folderPath,
-            SearchPattern = searchPattern
+            InboundFolder = folderPath,
+            SearchPattern = searchPattern,
+            ProcessFolder = archive
         };
 
     }
